2191c111992b858a1053d513fe9cb252a0c5226e
Manavk2004 <mjkamdar04@gmail.com>
2025-09-27 19:35:08 -0400

    Widget Loading

--- apps/widget/modules/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/ui/screens/widget-auth-screen.tsx
@@ -15,7 +15,9 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
-import { Doc } from "@workspace/backend/_generated/dataModel"
+import { Doc, Id } from "@workspace/backend/_generated/dataModel"
+import { contactSessionIdAtomFamily, organizationIdAtom } from "../atoms/widget-atoms"
+import { useAtomValue, useSetAtom } from "jotai"
 
 const formSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -25,6 +27,11 @@ const formSchema = z.object({
 const organizationId = "123"
 
 function WidgetAuthScreen() {
+    const organizationId = useAtomValue(organizationIdAtom)
+    const setContactSessionId = useSetAtom(
+        contactSessionIdAtomFamily(organizationId || "")
+    )
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -60,7 +67,7 @@ function WidgetAuthScreen() {
             metadata
         })
 
-        console.log({ contactSessionId });
+        setContactSessionId(contactSessionId);
     }
 
     return (
--- apps/widget/modules/ui/views/widget-view.tsx
+++ apps/widget/modules/ui/views/widget-view.tsx
@@ -2,6 +2,8 @@
 import { screenAtom } from "../atoms/widget-atoms";
 import WidgetAuthScreen from "../screens/widget-auth-screen";
 import { useAtomValue } from "jotai";
+import { WidgetErrorScreen } from "../screens/widget-error-screen";
+import { WidgetLoadingScreen } from "../screens/widget-loading-screen";
 interface Props {
     organizationId: string;
 }
@@ -10,8 +12,8 @@ export const WidgetView = ({ organizationId }: Props) => {
     const screen = useAtomValue(screenAtom)
 
     const screenComponents = {
-        error: <p>TODO: Error</p>,
-        loading: <p>TODO: Loading</p>,
+        loading: <WidgetLoadingScreen organizationId={organizationId}/>,
+        error: <WidgetErrorScreen />,
         auth: <WidgetAuthScreen />,
         voice: <p>TODO: Voice</p>,
         inbox: <p>TODO: Inbox</p>,
